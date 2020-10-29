97ff3cf984b106b26984b86914f8261297a9706a
DenisAvilov <avilovd.a@gmail.com>
2020-10-29 22:20:21 +0200

    Updates

---
+++ precache-manifest.4abeb9133a77397cdce199e93640ff64.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d2994e40029b8093453b7dfe3cc2d7b",
+    "url": "/mini-exemple-app-nova-kraina/index.html"
+  },
+  {
+    "revision": "89b2722271e9f154eaa0",
+    "url": "/mini-exemple-app-nova-kraina/static/css/main.b59563f1.chunk.css"
+  },
+  {
+    "revision": "4a2d339a8c843cf54ff7",
+    "url": "/mini-exemple-app-nova-kraina/static/js/2.0333158f.chunk.js"
+  },
+  {
+    "revision": "7a027744581ad182eb9c51f4f432b94a",
+    "url": "/mini-exemple-app-nova-kraina/static/js/2.0333158f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89b2722271e9f154eaa0",
+    "url": "/mini-exemple-app-nova-kraina/static/js/main.f7a6a3d0.chunk.js"
+  },
+  {
+    "revision": "70a8ea52cec19cf7d16b",
+    "url": "/mini-exemple-app-nova-kraina/static/js/runtime-main.7015c43a.js"
+  },
+  {
+    "revision": "4201152f9fb67330d18771c8ce89cad5",
+    "url": "/mini-exemple-app-nova-kraina/static/media/fantasy.4201152f.jpg"
+  },
+  {
+    "revision": "acdea94f71ca4873f9abe034d8d76039",
+    "url": "/mini-exemple-app-nova-kraina/static/media/field.acdea94f.jpg"
+  },
+  {
+    "revision": "6bc32f9f94c47e3a9d57e20fb1160506",
+    "url": "/mini-exemple-app-nova-kraina/static/media/finskiy_1280.6bc32f9f.jpg"
+  },
+  {
+    "revision": "e232cd9d8f9495c51dfc2a75ff6b0511",
+    "url": "/mini-exemple-app-nova-kraina/static/media/fon2mini.e232cd9d.jpg"
+  },
+  {
+    "revision": "7ed2b5927739bec853a2289d6484506a",
+    "url": "/mini-exemple-app-nova-kraina/static/media/handsome.7ed2b592.png"
+  },
+  {
+    "revision": "ed1f414b70277f61d97d9313ac28ecc8",
+    "url": "/mini-exemple-app-nova-kraina/static/media/learn_main_page.ed1f414b.jpg"
+  },
+  {
+    "revision": "3b25828cfdff6c0571d8ac042be7f926",
+    "url": "/mini-exemple-app-nova-kraina/static/media/media.3b25828c.jpg"
+  },
+  {
+    "revision": "d9e2bbeec4a68e03ff1ed35e9d8f7921",
+    "url": "/mini-exemple-app-nova-kraina/static/media/pamatka.d9e2bbee.png"
+  },
+  {
+    "revision": "a47ebfdeea541e3422343fe2b07e66e8",
+    "url": "/mini-exemple-app-nova-kraina/static/media/road.a47ebfde.jpg"
+  },
+  {
+    "revision": "55a7230b0d47a8542ff283ea4bfee94c",
+    "url": "/mini-exemple-app-nova-kraina/static/media/schoolUkraine.55a7230b.jpg"
+  },
+  {
+    "revision": "1d6706c2d4adac63008362015bae1dc0",
+    "url": "/mini-exemple-app-nova-kraina/static/media/survey-1.1d6706c2.jpg"
+  },
+  {
+    "revision": "f957b1bf8cdcf1632287e3f54ae1a821",
+    "url": "/mini-exemple-app-nova-kraina/static/media/tezis1.f957b1bf.jpg"
+  },
+  {
+    "revision": "966192d32f54d3f537ce525d3a78a868",
+    "url": "/mini-exemple-app-nova-kraina/static/media/ukraine.966192d3.jpg"
+  },
+  {
+    "revision": "171fda19e591c5553e52924ec08cd807",
+    "url": "/mini-exemple-app-nova-kraina/static/media/unsplash.171fda19.jpg"
+  },
+  {
+    "revision": "83694ee8a43ffae567923fb596423767",
+    "url": "/mini-exemple-app-nova-kraina/static/media/voidOnline.83694ee8.png"
+  }
+]);
